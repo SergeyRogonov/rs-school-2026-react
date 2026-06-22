05181e06cc8d28ded6905ba73b7f817b204af73b
Sergey Rogonov <serega.rogonov@gmail.com>
2026-06-22 14:51:18 +0300

    feat: update links to use next-intl createNavigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
+import { Link, useRouter } from '../i18n/navigation';
 import { useTheme } from '../context/useTheme';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import LocaleSwitcher from './LocaleSwitcher';
